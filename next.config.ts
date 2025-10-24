296e70835e8b6efe3c6179634c0a900912ed1ba2
hudo-ng <hmdong901026@gmail.com>
2025-10-24 12:29:19 -0600

    fix build error v3.1

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    disableTsconfigModification: true, // 👈 prevents Next.js from adding .next/types again
-  },
-  typescript: {
-    ignoreBuildErrors: false, // still enforce type checking
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
